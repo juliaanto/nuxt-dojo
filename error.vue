cd807ad2b8cf39abe1d4785e749a727aecfa47a6
Julia Antonova <julia.antonova19@gmail.com>
2024-01-11 13:21:35 +0100

    Add error page

---
+++ error.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col class="text-center">
+          <h1 class="text-h3 mt-5">{{ error.statusCode }}</h1>
+          <p class="text-h4 mt-3">
+            Oops!
+          </p>
+          <p class="mt-3">
+            {{ error.message }}
+          </p>
+          <v-btn @click="handleClearError" color="teal-accent-4" class="mt-3" outlined>
+            Go Home...
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script setup>
+  defineProps(['error'])
+
+  const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+<style scoped>
+
+</style>
