dc96816dc1c8ae8662050b8dfcf586f78e590cd6
Julia Antonova <julia.antonova19@gmail.com>
2024-01-24 12:10:46 +0100

    Fix types in API handler

--- server/api/currency/[code].ts
+++ server/api/currency/[code].ts
@@ -1,10 +1,19 @@
-export default defineEventHandler(async (event: any) => {
-  const { code } = event.context.params;
+interface ApiResponse {
+  data: {
+    [currencyCode: string]: {
+      code: string;
+      value: number;
+    };
+  };
+}
+
+export default defineEventHandler(async (event) => {
+  const code = getRouterParam(event, 'code');
   const { currencyKey } = useRuntimeConfig();
 
   const uri: string = `https://api.currencyapi.com/v3/latest?apikey=${currencyKey}&currencies=${code}&base_currency=EUR`;
 
-  const { data }: any = await $fetch(uri);
+  const { data } = await $fetch(uri) as ApiResponse;
 
   return data;
 })
